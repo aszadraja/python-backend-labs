f3049906458deda6f9951a94935e13e2cf5c4b85
aszadraja <aszadraja039@gmail.com>
2026-01-29 22:42:34 +0530

    Add array basic example

---
+++ day01_basics/array.py
@@ -0,0 +1,25 @@
+"""
+File: basics.py
+Day: 01
+Topic: Python Basics
+Author: Aszad Raja
+Description:
+    Practicing Arrays.
+"""
+
+# Status: Day01 Completed
+
+"""An array is a data structure that stores multiple values of 
+the same type in a single variable, accessed using an index."""
+
+arr = [123,234,345]
+
+for i in arr:
+    print(i)
+
+print(arr[0])
+print(arr[1])
+print(arr[2])
+
+if __name__ == "__main__":
+    print("Day 01 execution completed successfully.")
