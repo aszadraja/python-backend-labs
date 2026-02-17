72ff3ef0b7720fb5fdc4543dc644e5d9c60dc56e
aszadraja <aszadraja039@gmail.com>
2026-02-17 23:54:25 +0530

    Add logic of Odd and Prime numbers

--- day08_functions_collections/list_functions.py
+++ day08_functions_collections/list_functions.py
@@ -23,10 +23,35 @@ def count_even(numbers):
             count += 1
     return count
 
+def count_odd(numbers):
+    count = 0
+    for num in numbers:
+        if num %2 != 0:
+            count +=1
+    return count
+    
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+def find_primes(numbers):
+    primes = []
+    for num in numbers:
+        if is_prime(num):
+            primes.append(num)
+    return primes
+
 nums = [3, 7, 2, 9, 4]
 
 print("Max:", find_max(nums))
 print("Even count:", count_even(nums))
+print("Odd count:", count_odd(nums))
+print("Prime count:", find_primes(nums))
+
 
 if __name__ == "__main__":
     print("List functions executed successfully.")
